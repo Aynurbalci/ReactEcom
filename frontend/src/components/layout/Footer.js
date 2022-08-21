d5794ef7a9aab8e3e1a49362d72a725d4b733b3c
Broadcastage <64687379+Aynurbalci@users.noreply.github.com>
2022-08-21 10:47:10 +0300

    frontend

---
+++ frontend/src/components/layout/Footer.js
@@ -0,0 +1,14 @@
+import React, { Fragment } from 'react';
+
+export const Footer = () => {
+    return (
+        <Fragment>
+            <footer class="py-1">
+                <p className="text-center  mt-1">
+                    Shopping Cart - 2022, All Rights Reserved
+                </p>
+            </footer>
+        </Fragment>
+    );
+};
+export default Footer;
